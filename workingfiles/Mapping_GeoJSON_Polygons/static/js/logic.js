29748bb9e9adda852fa5df09c978af4435762284
Justin K <justinhkiang@gmail.com>
2022-04-20 16:34:20 -0700

    files reversed - fix and also remove multiple config.js files

--- workingfiles/Mapping_GeoJSON_Polygons/static/js/logic.js
+++ workingfiles/Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -2,14 +2,14 @@
 console.log("Working");
 
 // create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: mapbox_api_key
 });
 
 // create dark view (second tile layer) that will be an option for our map
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: mapbox_api_key
@@ -17,15 +17,15 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // create a base layer that holds both versions of the map
 let baseMaps = {
-    Day: light,
-    Night: dark
+    'Street': streets,
+    'Satellite Street': satelliteStreets
 };
 
 // create map object with center, zoom level and default layer
 let map = L.map('mapid', {
-    center: [44, -80],
-    zoom: 2,
-    layers: [dark]
+    center: [43.68, -79.39],
+    zoom: 11,
+    layers: [satelliteStreets]
 });
 
 // pass our map layers into our layers control and add the layers control to the map
@@ -33,24 +33,25 @@ L.control.layers(baseMaps).addTo(map);
 
 // do this AFTER titleLayer so that the map loads before we try and load individual data points
 // pull airport data from my own Repo on GitHub
-let torontoData = "https://raw.githubusercontent.com/JustGitCoding/Earthquake_Mapping/main/workingfiles/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/JustGitCoding/Earthquake_Mapping/main/workingfiles/torontoNeighborhoods.json";
 
 
 // create line style (so code below isn't as cluttered)
 let myStyle = {
-    color: "#ffffa1",
-    weight: 1
+    color: "blue",
+    weight: 1,
+    fillColor: "yellow",
+    fillOpacity: 0.3
 };
 
 // grabbing GeoJSON data --- MULTIPLE data points
-d3.json(torontoData).then(function(data){
+d3.json(torontoHoods).then(function(data){
     console.log(data);
 // Creating a GeoJSON layer with the retrieved data (MULTIPLE points)
 L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2><hr><h3>Destination: " 
-        + feature.properties.dst + "</h3>");
+        layer.bindPopup("<h2> "+feature.properties.AREA_NAME+"</h2>")
     }
 }).addTo(map);
 });
