1ddbada8cfa1f33b4dfbdb1bc7fa4699dc598a69
Justin K <justinhkiang@gmail.com>
2022-04-20 14:01:03 -0700

    mapping geojson data

--- workingfiles/Mapping_GeoJSON_Points/static/js/logic.js
+++ workingfiles/Mapping_GeoJSON_Points/static/js/logic.js
@@ -8,8 +8,32 @@ console.log("Working");
 // });
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// set view to see the lines we draw below
-let map = L.map('mapid').setView([33.9416, -118.4085], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+// create a geoJSON 'layer' to our map (not needed since we can chain below)
+// L.geoJSON(sanFranAirport).addTo(map);
 
 // create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -21,59 +45,21 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 // add 'graymap' tile layer to the map
 streets.addTo(map);
 
-// add a 'marker' to the map for Los Angeles
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// // using pointToLayer
+// L.geoJSON(sanFranAirport, {
+//     pointToLayer: function(feature, latlng) {
+//         console.log(feature);
+//         return L.marker(latlng).bindPopup(
+//             "<h2>" + feature.properties.name + "</h2><hr>" + "<h3>" + feature.properties.city +", "+ feature.properties.country + "</h3>");
+//     }
+// }).addTo(map);
 
-let circle = L.circle([34.0522, -118.2437], {
-    color: 'yellow',
-    radius: 300
-}).addTo(map);
 
-let circleMarker = L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'red',
-}).addTo(map);
-
-// pull cities data from cities.js
-let cityData = cities;
-
-// loop through the array of objects and create a marker for each city
-// <hr> adds a line break in our popup
-// toLocaleString() adds a thousands comma separator
-cityData.forEach(city => {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        weight: 4
-    }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>").addTo(map)
-});
-
-// draw a line from LAX to SFO
-
-// coordinates for the line
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-
-// create a polyline useing the line coordinates above and make the line red
-L.polyline(line, {
-    color: "yellow"
-}).addTo(map);
-
-let line2 = [
-    [33.9416, -118.4085],
-    [41.8781, -87.6298],
-    [29.7604, -95.3698],
-    [40.6413, -73.7781]
-  ];
-
-// create a polyline useing the line coordinates above and make the line red
-L.polyline(line2, {
-    color: "blue",
-    weight: 4,
-    opacity: 0.5,
-    dashArray: '10, 10', dashOffset: '10'
+// using onEachFeature
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup(
+            "<h2>Airport Code: " + layer.feature.properties.faa + "</h2><hr>" + "<h3>Airport Name: " + layer.feature.properties.name + "</h3>");
+    }
 }).addTo(map);
